66d72890217a91b721dba7783f7959cde42337e5
kjcodingsheesh <kjgamingpogs@gmail.com>
2026-01-18 21:35:52 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 // Serve static files
 app.use(express.static('public'));
